170867fdd769fc27b1f59b31593f4b8b6dd8e6d4
jina <psalvarajah@lcs.on.ca>
2020-11-09 12:06:55 -0500

    Initial Commit

---
+++ MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by Salvarajah, Prajina on 2020-11-09.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
