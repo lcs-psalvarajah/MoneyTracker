647310eb45f32f427e07f4950a8b11367c5330ae
jina <psalvarajah@lcs.on.ca>
2020-11-20 11:35:13 -0500

    Described a basic purchase category

--- MoneyTracker/MoneyTrackerApp.swift
+++ MoneyTracker/MoneyTrackerApp.swift
@@ -15,3 +15,4 @@ struct MoneyTrackerApp: App {
         }
     }
 }
+
---
+++ MoneyTracker/PurchaseCategory.swift
@@ -0,0 +1,18 @@
+
+//  File.swift
+//  MoneyTracker
+//
+//  Created by Salvarajah, Prajina on 2020-11-20.
+//
+
+import Foundation
+
+struct PurchaseCategory: Identifiable {
+    var id = UUID()
+    var title: String
+    var description: String
+    
+}
+
+
+
